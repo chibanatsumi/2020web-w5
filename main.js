d12228579f46a1c184f014c4fe322ffde195871b
chibanatsumi <75403478+chibanatsumi@users.noreply.github.com>
2021-01-03 18:34:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = '気温華氏４０度で外は寒かった。 :insertx: は出かけた。 :inserty: に着いた。 :insertz:。 Bobは驚いたが、体重300ポンドの :insertx: は驚かなかった。 :inserty: ではよくあることだった。';
 let insertX = ['翔','智','和也','潤','雅紀'];
 let insertY = ['東京ドーム','ディズニーランド','家'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let insertZ = ['コンサートをやっていた','デートしていた','踊っていた'];
 
 randomize.addEventListener('click', result);
 
