fe4e2c919c121913e5939e8892930cfa87c4a4a6
chibanatsumi <75403478+chibanatsumi@users.noreply.github.com>
2021-01-03 19:25:20 +0900

    Update main-ja.js

--- main-ja.js
+++ main-ja.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = '気温 華氏４０度で外は寒かった。 :insertx: は出かけた。  :inserty: に着いた。  :insertz:. 体重110ポンドのBobは驚いたが、 :insertx: は驚かなかった。 :inserty: ではよくあることだった。';
 let insertX = ['智','翔','雅紀','和也','潤'];
 let insertY = ['東京ドーム','ディズニーランド','家'];
 let insertZ = ['コンサートをしていた','デートしていた','踊っていた'];
